e1e2a2950816e21ea42d81490a5c9948f30ca49e
ajl1143 <ajl1143@wildcats.unh.edu>
2023-10-30 19:16:50 -0400

    Changed to run on our server

--- client/src/main/java/edu/unh/cs/cs619/bulletzone/rest/BulletZoneRestClient.java
+++ client/src/main/java/edu/unh/cs/cs619/bulletzone/rest/BulletZoneRestClient.java
@@ -24,8 +24,8 @@ import edu.unh.cs.cs619.bulletzone.util.LongWrapper;
  */
 
 //@Rest(rootUrl = "http://10.21.159.62:8080/games",
-@Rest(rootUrl = "http://stman1.cs.unh.edu:6192/games",
-//@Rest(rootUrl = "http://stman1.cs.unh.edu:61902/games",
+//@Rest(rootUrl = "http://stman1.cs.unh.edu:6192/games",
+@Rest(rootUrl = "http://stman1.cs.unh.edu:61906/games",
         converters = {StringHttpMessageConverter.class, MappingJackson2HttpMessageConverter.class}
         // TODO: disable intercepting and logging
         // , interceptors = { HttpLoggerInterceptor.class }
