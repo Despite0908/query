7022141ce6a8a4992b5edacbbb34f7ed42a2fdb7
ajl1143 <ajl1143@wildcats.unh.edu>
2023-12-08 20:12:22 -0500

    Changes to finish up PowerUpPickUpTests.java

--- server/src/test/java/edu/unh/cs/cs619/bulletzone/repository/PowerUpPickUpTest.java
+++ server/src/test/java/edu/unh/cs/cs619/bulletzone/repository/PowerUpPickUpTest.java
@@ -2,8 +2,10 @@ package edu.unh.cs.cs619.bulletzone.repository;
 
 import static org.mockito.Mockito.when;
 
+import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
@@ -37,7 +39,10 @@ public class PowerUpPickUpTest {
         repo = new InMemoryGameRepository();
         repo.setMapPath("PowerUpTestMap.json");
         repo.setTankSpawn(15, 0);
+        repo.injectClock(mockClock);
     }
+
+
 
     /**
      * pickupAntiGravTestMovementModifierTest -- tests that the
@@ -68,6 +73,62 @@ public class PowerUpPickUpTest {
         Assert.assertEquals(repo.move(tank.getId(), Direction.Up), 1);
         long newFiringInterval = tank.getAllowedFireInterval();
         Assert.assertEquals(600, newFiringInterval);
+    }
+
+    /**
+     * pickupAntiGravTestMovementModifierTest -- tests that the
+     * movement speed is correct after getting an AntiGrav PowerUp.
+     * @throws Exception
+     */
+    @Test
+    public void pickupMultipleAntiGravTestMovementModifierTest() throws Exception {
+        when(mockClock.millis()).thenReturn((long)500);
+        Tank tank = repo.join("");
+        Assert.assertNotNull(tank);
+        long thisMillis = mockClock.millis();
+        Assert.assertEquals(1, repo.move(tank.getId(), Direction.Up));
+        when(mockClock.millis()).thenReturn((long)1500);
+        repo.turn(tank.getId(), Direction.Right);
+        when(mockClock.millis()).thenReturn((long)2500);
+        long thisMillis2 = mockClock.millis();
+        Assert.assertEquals(1, repo.move(tank.getId(), Direction.Right));
+        when(mockClock.millis()).thenReturn((long)3500);
+        repo.turn(tank.getId(), Direction.Up);
+        when(mockClock.millis()).thenReturn((long)4500);
+        Assert.assertEquals(1, repo.move(tank.getId(), Direction.Up));
+        when(mockClock.millis()).thenReturn((long)5500);
+        repo.turn(tank.getId(), Direction.Left);
+        when(mockClock.millis()).thenReturn((long)6500);
+        Assert.assertEquals(1, repo.move(tank.getId(), Direction.Left));
+        long newMovementInterval = tank.getAllowedMoveInterval();
+        Assert.assertEquals(195, newMovementInterval);
+    }
+
+    /**
+     * pickupAntiGravTestMovementModifierTest -- tests that the
+     * movement speed is correct after getting an AntiGrav PowerUp.
+     * @throws Exception
+     */
+    @Test
+    public void pickupMultipleAntiGravTestMovementModifierTest2() throws Exception {
+        when(mockClock.millis()).thenReturn((long)500);
+        Tank tank = repo.join("");
+        Assert.assertNotNull(tank);
+        long thisMillis = mockClock.millis();
+        Assert.assertEquals(1, repo.move(tank.getId(), Direction.Up));
+        when(mockClock.millis()).thenReturn((long)1500);
+        long thisMillis2 = mockClock.millis();
+        Assert.assertEquals(1, repo.move(tank.getId(), Direction.Up));
+        when(mockClock.millis()).thenReturn((long)3500);
+        repo.turn(tank.getId(), Direction.Right);
+        when(mockClock.millis()).thenReturn((long)4500);
+        Assert.assertEquals(1, repo.move(tank.getId(), Direction.Right));
+        when(mockClock.millis()).thenReturn((long)5500);
+        repo.turn(tank.getId(), Direction.Down);
+        when(mockClock.millis()).thenReturn((long)6500);
+        Assert.assertEquals(1, repo.move(tank.getId(), Direction.Down));
+        long newMovementInterval = tank.getAllowedMoveInterval();
+        Assert.assertEquals(195, newMovementInterval);
     }
 
 }
